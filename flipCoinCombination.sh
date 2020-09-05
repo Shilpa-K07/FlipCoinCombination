55ed96b0000a4a0818160e5b055e417e06813372
shilpa <shilpa07udupi@gmail.com>
2020-09-05 16:55:59 +0530

    finding singlet percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,33 @@
 echo welcome to flip coin combination problem !
 
-randomNum=$((RANDOM%2))
+i=0
+declare -A dictionary
 
-if [[ $randomNum -eq 0 ]]
-then
-	echo HEAD
-else
-	echo TAIL
-fi
+while [[ $i -lt 20 ]]
+do
+	randomNum=$((RANDOM%2))
+	dictionary[$i]=$randomNum
+	i=$(($i+1))
+done
+
+head=0
+tail=0
+
+for val in ${dictionary[@]}
+do
+	if [[ $val -eq 0 ]]
+	then
+		((head++))
+	else
+		((tail++))
+	fi
+done
+
+echo head is $head
+echo tail is $tail
+
+head=$((100*$head/20))
+tail=$((100*$tail/20))
+
+echo percentage of head is $head
+echo percentage of tail is $tail
