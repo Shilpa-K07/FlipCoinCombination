aac9b52bf933e0ed76fb7e71d919154690ca30fb
shilpa <shilpa07udupi@gmail.com>
2020-09-05 17:39:26 +0530

    calculating doublet percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,41 +1,45 @@
+
 echo welcome to flip coin combination problem !!
 
-if [[ $randomNum -eq 0 ]]
-then
-	echo HEAD
-else
-	echo TAIL
-fi
-
-echo multiple time flipping and finding singlet percentage
+echo multiple time flipping and finding doublet percentage
 i=0
 declare -A dictionary
+HH=0
+HT=0
+TH=0
+TT=0
 
 while [[ $i -lt 20 ]]
 do
-	randomNum=$((RANDOM%2))
-	dictionary[$i]=$randomNum
+	randomNum=$((RANDOM%4))
+	if [[ $randomNum -eq 0 ]]
+	then
+		dictionary[$i]=HH
+		((HH++))
+	elif [[ $randomNum -eq 1 ]]
+	then
+		dictionary[$i]=HT
+		((HT++))
+	elif [[ $randomNum -eq 2 ]]
+	then
+		dictionary[$i]=TH
+		((TH++))
+	else
+		dictionary[$i]=TT
+		((TT++))
+	fi
 	i=$(($i+1))
 done
 
-head=0
-tail=0
+echo doublet combination is ${dictionary[@]}
+HH=$((100*$HH/20))
+HT=$((100*$HT/20))
+TH=$((100*$TH/20))
+TT=$((100*$TT/20))
 
-for val in ${dictionary[@]}
-do
-	if [[ $val -eq 0 ]]
-	then
-		((head++))
-	else
-		((tail++))
-	fi
-done
 
-echo head is $head
-echo tail is $tail
+declare -A percentageDict
 
-head=$((100*$head/20))
-tail=$((100*$tail/20))
-
-echo percentage of head is $head
-echo percentage of tail is $tail
+percentageDict=([HH]=$HH [HT]=$HT [TH]=$TH [TT]=$TT)
+echo storing doublet percentage in dictionary
+echo ${percentageDict[@]}
