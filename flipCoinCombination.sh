4d0c66e415f9000f3cbb27a25c26547600158bfd
shilpa <shilpa07udupi@gmail.com>
2020-09-06 10:42:53 +0530

    Sorting combinations

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,33 +1,125 @@
 echo welcome to flip coin combination problem !
 
 i=0
-declare -A dictionary
-
+H=0
+T=0
 while [[ $i -lt 20 ]]
 do
-	randomNum=$((RANDOM%2))
-	dictionary[$i]=$randomNum
+   randomNum=$((RANDOM%2)) 
+	if [[ $randomNum -eq 0 ]] 
+	then 
+		((H++))
+	else 
+		((T++))
+	fi
 	i=$(($i+1))
 done
 
-head=0
-tail=0
+HH=0
+HT=0
+TH=0
+TT=0
 
-for val in ${dictionary[@]}
+i=0
+while [[ $i -lt 20 ]]
 do
-	if [[ $val -eq 0 ]]
+	randomNum=$((RANDOM%4))
+	if [[ $randomNum -eq 0 ]]
 	then
-		((head++))
+		((HH++))
+	elif [[ $randomNum -eq 1 ]]
+	then
+		((HT++))
+	elif [[ $randomNum -eq 2 ]]
+	then
+		((TH++))
 	else
-		((tail++))
+		((TT++))
 	fi
+	i=$(($i+1))
 done
 
-echo head is $head
-echo tail is $tail
+HHH=0
+HHT=0
+HTT=0
+HTH=0
+TTT=0
+TTH=0
+THH=0
+THT=0
 
-head=$((100*$head/20))
-tail=$((100*$tail/20))
+i=0
+while [[ $i -lt 20 ]]
+do
+	randomNum=$((RANDOM%8))
+	if [[ $randomNum -eq 0 ]]
+	then
+		((HHH++))
+	elif [[ $randomNum -eq 1 ]]
+	then
+		((HHT++))
+	elif [[ $randomNum -eq 2 ]]
+	then
+		((HTT++))
+	elif [[ $randomNum -eq 3 ]]
+	then
+		((HTH++))
+	elif [[ $randomNum -eq 4 ]]
+        then
+		((TTT++))
+	elif [[ $randomNum -eq 5 ]]
+        then
+		((TTH++))
+	elif [[ $randomNum -eq 6 ]]
+        then
+		((THH++))
+	elif [[ $randomNum -eq 7 ]]
+        then
+		((THT++))
+	fi
+	i=$(($i+1))
+done
 
-echo percentage of head is $head
-echo percentage of tail is $tail
+declare -A sortingDictionary
+sortingDictionary=([H]=$H [T]=$T [HH]=$HH [HT]=$HT [TH]=$TH [TT]=$TT [HHH]=$HHH [HHT]=$HHT [HTT]=$HTT [HTH]=$HTH [TTT]=$TTT [TTH]=$TTH [THH]=$THH [THT]=$THT) 
+
+echo "Dictionary Insertion Order:" $H $T $HH $HT $TH $TT $HHH $HHT $HTT $HTH $TTT $TTH $THH $THT
+
+n=${#sortingDictionary[@]}
+
+i=0
+for val in ${sortingDictionary[@]}
+do
+   arr[i]=$val
+   i=$(($i+1))
+done
+
+n=${#arr[@]}
+for((i=0;i<$n;i++))
+    do
+        for((j=((i+1));j<$n;j++))
+        do
+            if [ ${arr[i]} -gt ${arr[j]} ]
+            then
+                tmp=${arr[i]}
+                arr[i]=${arr[j]}
+                arr[j]=$tmp
+            fi
+        done
+done
+
+echo "dictionary:" ${sortingDictionary[@]}
+echo "Array:" ${arr[@]}
+maxValue=${arr[$(($n-1))]}
+echo "max value " $maxValue
+
+i=0
+for key in ${!sortingDictionary[@]}
+do
+   if [ ${sortingDictionary[$key]} -eq $maxValue ]
+    then
+	combination[$i]=$key
+	i=$(($i+1))
+   fi
+done
+echo "Winning combination is "${combination[@]}
